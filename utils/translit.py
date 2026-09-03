c29ca0bd8ad03f050adbed0e416d720dbcaabf06
PurpleSwtr <sergeenko.mihail@yandex.ru>
2026-09-04 02:05:37 +0300

    added translit tool

---
+++ utils/translit.py
@@ -0,0 +1,40 @@
+def translit(text: str) -> str:
+    rules = {
+        "а": "a",
+        "б": "b",
+        "в": "v",
+        "г": "g",
+        "д": "d",
+        "е": "e",
+        "ё": "yo",
+        "ж": "zh",
+        "з": "z",
+        "и": "i",
+        "й": "y",
+        "к": "k",
+        "л": "l",
+        "м": "m",
+        "н": "n",
+        "о": "o",
+        "п": "p",
+        "р": "r",
+        "с": "s",
+        "т": "t",
+        "у": "u",
+        "ф": "f",
+        "х": "h",
+        "ц": "ts",
+        "ч": "ch",
+        "ш": "sh",
+        "щ": "sch",
+        "ъ": "",
+        "ы": "y",
+        "ь": "",
+        "э": "e",
+        "ю": "yu",
+        "я": "ya",
+    }
+
+    full_rules = {**rules, **{k.upper(): v.title() for k, v in rules.items()}}
+
+    return text.translate(str.maketrans(full_rules))
